64c9bc9f0d9e2f124baa870a7f0f0cabfc86f7be
Albert <albert.apsit@ericsson.com>
2021-11-16 17:15:26 +0200

    ADD: specs, specs-derive

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use rltk::{GameState, Rltk};
+use specs::prelude::*;
+use specs_derive::Component;
+use std::cmp::{max, min};
+
+struct State {}
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello Rust World");
+    }
+}
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build()?;
+    let gs = State {};
+    rltk::main_loop(context, gs)
 }
