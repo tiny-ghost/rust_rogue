8236a26ee2e070c5ee32263a1a9e4511b143c257
Albert <albert.apsit@ericsson.com>
2022-01-16 18:53:54 +0200

    UPD: Finish chapter 3

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,15 @@ use specs_derive::Component;
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         player_input(self, ctx);
+
         ctx.cls();
+
         let positions = self.ecs.read_storage::<Position>();
+
         let renderables = self.ecs.read_storage::<Renderable>();
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
+
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
         }
@@ -31,6 +37,8 @@ fn main() -> rltk::BError {
 
     gs.ecs.register::<Renderable>();
 
+    gs.ecs.insert(new_map());
+
     gs.ecs
         .create_entity()
         .with(Position { x: 40, y: 25 })
@@ -50,9 +58,16 @@ fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
 
     let mut players = ecs.write_storage::<Player>();
 
+    let map = ecs.fetch::<Vec<TileType>>();
+
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
+        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(79, max(0, pos.x + delta_x));
+
+            pos.y = min(49, max(0, pos.y + delta_y));
+        }
     }
 }
 
@@ -69,8 +84,72 @@ fn player_input(gs: &mut State, ctx: &mut Rltk) {
     }
 }
 
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * 80) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80 * 50];
+
+    for x in 0..80 {
+        map[(xy_idx(x, 0))] = TileType::Wall;
+        map[(xy_idx(x, 49))] = TileType::Wall;
+    }
+    for y in 0..50 {
+        map[(xy_idx(0, y))] = TileType::Wall;
+        map[(xy_idx(79, y))] = TileType::Wall;
+    }
+
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+    map
+}
+
+fn draw_map(map: &[TileType], ctx: &mut Rltk) {
+    let mut y = 0;
+    let mut x = 0;
+
+    for tile in map.iter() {
+        match tile {
+            TileType::Floor => ctx.set(
+                x,
+                y,
+                RGB::from_f32(0.5, 0.5, 0.5),
+                RGB::from_f32(0., 0., 0.),
+                rltk::to_cp437('.'),
+            ),
+            TileType::Wall => ctx.set(
+                x,
+                y,
+                RGB::from_f32(0.5, 1.0, 0.5),
+                RGB::from_f32(0., 0., 0.),
+                rltk::to_cp437('#'),
+            ),
+        }
+
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
+}
 struct State {
     ecs: World,
+}
+
+#[derive(PartialEq, Clone, Copy, Component)]
+enum TileType {
+    Wall,
+    Floor,
 }
 
 #[derive(Component)]
